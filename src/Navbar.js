4a72ddb73aeba19e6872acdf9fe04765dd05969e
Ujjwal Gupta <guptaujjwal600@gmail.com>
2024-12-10 22:59:26 +0530

    third commit

---
+++ src/Navbar.js
@@ -0,0 +1,26 @@
+import "./index.css";
+function Navbar() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="left-nav">
+          <image></image>
+          <h>Savile & Co</h>
+        </div>
+        <div className="right-nav">
+          <ul>
+            <li>New Arrivals</li>
+            <li>Clothing</li>
+            <li>Shoes</li>
+            <li>Accessories</li>
+            <li>Grooming</li>
+            <li>Sale</li>
+            <li>Help</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
